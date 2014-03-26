a96d88429ad0637c851d87dc487425a9b3b67dfe
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:07:08 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/giflib/build-osx32.sh
+++ csrc/giflib/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 *.c -shared -o ../../bin/osx32/libgif.dylib -I.
+gcc -arch i386 -O2 *.c -shared -install_name @loader_path/libgiflib.dylib -o ../../bin/osx32/libgif.dylib -I.
--- csrc/giflib/build-osx64.sh
+++ csrc/giflib/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 *.c -shared -o ../../bin/osx64/libgif.dylib -I.
+gcc -arch x86_64 -O2 *.c -shared -install_name @loader_path/libgiflib.dylib -o ../../bin/osx64/libgif.dylib -I.
